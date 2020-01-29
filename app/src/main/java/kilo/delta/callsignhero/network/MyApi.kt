2b577baf18a6f0b5a8861786c8addf43d916748d
Kilo Delta <kyle.richard.disalvo@gmail.com>
2020-01-29 12:02:40 -0500

    api fix

--- app/src/main/java/kilo/delta/callsignhero/network/MyApi.kt
+++ app/src/main/java/kilo/delta/callsignhero/network/MyApi.kt
@@ -1,27 +1,21 @@
 package kilo.delta.callsignhero.network
 
+import androidx.annotation.NonNull
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface MyApi {
 
-    @FormUrlEncoded
-    @POST("search")
-    fun userSearch(
-        @Field("callsign")callsign: String
-    ) : Call<ResponseBody>
-
+    @GET("{callsign}/json")
+    fun userSearch(@Path("callsign") @NonNull callsign: String) : Call<ResponseBody>
 
     companion object {
         operator fun invoke() : MyApi{
             return Retrofit.Builder()
-                    //TODO - need to make the callsign a param
-                .baseUrl("https://callook.info/w1aw/json")
+                .baseUrl("https://callook.info/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(MyApi::class.java)
